14259828df3acbe58df353f291651dce4e947753
Michael Adam <obnox@samba.org>
2020-05-20 00:10:17 +0200

    Add a SmbService API/CR scaffold
    Signed-off-by: Michael Adam <obnox@samba.org>

---
+++ pkg/apis/smbservice/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the smbservice v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=smbservice.samba.org
+package v1alpha1
---
+++ pkg/apis/smbservice/v1alpha1/smbservice_types.go
@@ -0,0 +1,48 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// SmbServiceSpec defines the desired state of SmbService
+type SmbServiceSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// SmbServiceStatus defines the observed state of SmbService
+type SmbServiceStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// SmbService is the Schema for the smbservices API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=smbservices,scope=Namespaced
+type SmbService struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SmbServiceSpec   `json:"spec,omitempty"`
+	Status SmbServiceStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// SmbServiceList contains a list of SmbService
+type SmbServiceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []SmbService `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&SmbService{}, &SmbServiceList{})
+}
