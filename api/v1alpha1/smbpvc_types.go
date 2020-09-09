35e6520ed7936a1d74b515954a2ea3c81c70b0ab
John Mulligan <jmulligan@redhat.com>
2020-09-09 16:17:13 -0400

    sdk: create kind for SmbPvc
    Command: operator-sdk-v1.0.0 create api --group=samba-operator --version=v1alpha1
    --kind=SmbPvc --resource --controller
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ api/v1alpha1/smbpvc_types.go
@@ -0,0 +1,64 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// SmbPvcSpec defines the desired state of SmbPvc
+type SmbPvcSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of SmbPvc. Edit SmbPvc_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// SmbPvcStatus defines the observed state of SmbPvc
+type SmbPvcStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// SmbPvc is the Schema for the smbpvcs API
+type SmbPvc struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SmbPvcSpec   `json:"spec,omitempty"`
+	Status SmbPvcStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// SmbPvcList contains a list of SmbPvc
+type SmbPvcList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []SmbPvc `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&SmbPvc{}, &SmbPvcList{})
+}
--- main.go
+++ main.go
@@ -75,6 +75,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "SmbService")
 		os.Exit(1)
 	}
+	if err = (&controllers.SmbPvcReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("SmbPvc"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "SmbPvc")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
