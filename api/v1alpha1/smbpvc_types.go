1bd552d5ac604b9e0e304e038f955557b8a430dd
John Mulligan <jmulligan@redhat.com>
2020-09-10 07:23:43 -0400

    sdk: port old operator SmbPvc support to v1.0 style
    Manually bring over code to support SmbPvc kind to
    smbpvc_types.go and smbpvc_controller.go.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- api/v1alpha1/smbpvc_types.go
+++ api/v1alpha1/smbpvc_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,8 +29,10 @@ type SmbPvcSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of SmbPvc. Edit SmbPvc_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// TODO: Unsure yet whether we need the PVC params embedded or under
+	// their own layer...
+	Pvc *corev1.PersistentVolumeClaimSpec `json:"pvc"`
+	//*corev1.PersistentVolumeClaimSpec `json:",inline"`
 }
 
 // SmbPvcStatus defines the observed state of SmbPvc
--- controllers/smbpvc_controller.go
+++ controllers/smbpvc_controller.go
@@ -20,11 +20,13 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	sambaoperatorv1alpha1 "github.com/obnoxxx/samba-operator/api/v1alpha1"
+	"github.com/obnoxxx/samba-operator/internal/resources"
 )
 
 // SmbPvcReconciler reconciles a SmbPvc object
@@ -37,17 +39,25 @@ type SmbPvcReconciler struct {
 // +kubebuilder:rbac:groups=samba-operator.samba.org,resources=smbpvcs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=samba-operator.samba.org,resources=smbpvcs/status,verbs=get;update;patch
 
+// Reconcile reads that state of the cluster for a SmbPvc object and makes changes based on the state read
 func (r *SmbPvcReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("smbpvc", req.NamespacedName)
+	ctx := context.Background()
+	reqLogger := r.Log.WithValues("smbpvc", req.NamespacedName)
+	reqLogger.Info("Reconciling SmbPvc")
 
-	// your logic here
-
-	return ctrl.Result{}, nil
+	smbPvcManager := resources.NewSmbPvcManager(r, r.Scheme, reqLogger)
+	res := smbPvcManager.Update(ctx, req.NamespacedName)
+	err := res.Err()
+	if res.Requeue() {
+		return ctrl.Result{Requeue: true}, err
+	}
+	return ctrl.Result{}, err
 }
 
 func (r *SmbPvcReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&sambaoperatorv1alpha1.SmbPvc{}).
+		Owns(&corev1.PersistentVolumeClaim{}).
+		Owns(&sambaoperatorv1alpha1.SmbService{}).
 		Complete(r)
 }
