129d2bcb844b484c76d1dad8c50837cbc7f9e180
John Mulligan <jmulligan@redhat.com>
2020-09-25 15:49:11 -0400

    resources: create an internal package for resource management
    Create an internal package for resource management that separates the
    "business logic" of the operator from the framework.
    Convert smbservice controller to the new package.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- pkg/controller/smbservice/smbservice_controller.go
+++ pkg/controller/smbservice/smbservice_controller.go
@@ -4,17 +4,13 @@ import (
 	"context"
 	//"reflect"
 
+	"github.com/obnoxxx/samba-operator/internal/resources"
 	smbservicev1alpha1 "github.com/obnoxxx/samba-operator/pkg/apis/smbservice/v1alpha1"
 
 	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -89,118 +85,12 @@ func (r *ReconcileSmbService) Reconcile(request reconcile.Request) (reconcile.Re
 	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
 	reqLogger.Info("Reconciling SmbService")
 
-	// Fetch the SmbService instance
-	instance := &smbservicev1alpha1.SmbService{}
-	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			// Request object not found, could have been deleted after reconcile request.
-			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
-			// Return and don't requeue
-			return reconcile.Result{}, nil
-		}
-		// Error reading the object - requeue the request.
-		return reconcile.Result{}, err
+	smbServiceManager := resources.NewSmbServiceManager(
+		r.client, r.scheme, reqLogger)
+	res := smbServiceManager.Update(context.TODO(), request.NamespacedName)
+	err := res.Err()
+	if res.Requeue() {
+		return reconcile.Result{Requeue: true}, err
 	}
-
-	// Check if the deployment already exists, if not create a new one
-	found := &appsv1.Deployment{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: instance.Name, Namespace: instance.Namespace}, found)
-	if err != nil && errors.IsNotFound(err) {
-		// not found - define a new deployment
-		dep := r.deploymentForSmbService(instance)
-		reqLogger.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
-		err = r.client.Create(context.TODO(), dep)
-		if err != nil {
-			reqLogger.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
-			return reconcile.Result{}, err
-		}
-		// Deployment created successfully - return and requeue
-		return reconcile.Result{Requeue: true}, nil
-	} else if err != nil {
-		reqLogger.Error(err, "Failed to get Deployment")
-		return reconcile.Result{}, err
-	}
-
-	// Ensure the deployment size is the same as the spec
-	var size int32 = 1
-	if *found.Spec.Replicas != size {
-		found.Spec.Replicas = &size
-		err = r.client.Update(context.TODO(), found)
-		if err != nil {
-			reqLogger.Error(err, "Failed to update Deployment", "Deployment.Namespace", found.Namespace, "Deployment.Name", found.Name)
-			return reconcile.Result{}, err
-		}
-		// Spec updated - return and requeue
-		return reconcile.Result{Requeue: true}, nil
-	}
-
-	return reconcile.Result{}, nil
-}
-
-// deploymentForSmbService returns a smbservice deployment object
-func (r *ReconcileSmbService) deploymentForSmbService(s *smbservicev1alpha1.SmbService) *appsv1.Deployment {
-	// labels - do I need them?
-	labels := labelsForSmbService(s.Name)
-	smb_volume := s.Spec.PvcName + "-smb"
-	var size int32 = 1
-
-	dep := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: s.Name,
-			// TODO: get namespace from pvc
-			Namespace: "default",
-			Labels:    labels,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: &size,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: labels,
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: labels,
-				},
-				Spec: corev1.PodSpec{
-					Volumes: []corev1.Volume{{
-						Name: smb_volume,
-						VolumeSource: corev1.VolumeSource{
-							PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
-								ClaimName: s.Spec.PvcName,
-							},
-						},
-					}},
-					Containers: []corev1.Container{{
-						Image: "quay.io/obnox/samba-centos8:latest",
-						Name:  "samba",
-						//NEEDED? - Command: []string{"cmd", "arg", "arg", "..."},
-						Ports: []corev1.ContainerPort{{
-							ContainerPort: 139,
-							Name:          "smb-netbios",
-						}, {
-							ContainerPort: 445,
-							Name:          "smb",
-						}},
-						VolumeMounts: []corev1.VolumeMount{{
-							MountPath: "/share",
-							Name:      smb_volume,
-						}},
-					}},
-				},
-			},
-		},
-	}
-
-	// set the smbservice instance as the owner and controller
-	controllerutil.SetControllerReference(s, dep, r.scheme)
-	return dep
-}
-
-// labelsForSmbService returns the labels for selecting the resources
-// belonging to the given smbservice CR name.
-func labelsForSmbService(name string) map[string]string {
-	return map[string]string{
-		"app":           "smbservice",
-		"smbservice_cr": name,
-	}
+	return reconcile.Result{}, err
 }
