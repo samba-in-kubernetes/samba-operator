a70fdcec9b7cb4ceced6dd467df8dcd13781c2a0
John Mulligan <jmulligan@redhat.com>
2021-02-04 16:37:56 -0500

    api: add a new SmbShare CRD that will replace the current ones
    The new SmbShare CRD, sketched out in the recent design doc, will
    be used for establishing what shares and desired versus defining
    servers directly. This only commits the results of the new type
    and supporting metadata based on the operator-sdk tool.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- main.go
+++ main.go
@@ -91,6 +91,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "SmbPvc")
 		os.Exit(1)
 	}
+	if err = (&controllers.SmbShareReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("SmbShare"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "SmbShare")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
