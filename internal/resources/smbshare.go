4783857c2229826be97d3e43a009c06ee068454f
John Mulligan <jmulligan@redhat.com>
2021-02-05 15:32:05 -0500

    resources: split smbshare update into sub-function chunks
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- internal/resources/smbshare.go
+++ internal/resources/smbshare.go
@@ -60,39 +60,73 @@ func (m *SmbShareManager) Update(ctx context.Context, nsname types.NamespacedNam
 		return Result{err: err}
 	}
 
-	// Check if the deployment already exists, if not create a new one
-	found := &appsv1.Deployment{}
-	err = m.client.Get(ctx, types.NamespacedName{Name: instance.Name, Namespace: instance.Namespace}, found)
-	if err != nil && errors.IsNotFound(err) {
-		// not found - define a new deployment
-		dep := m.deploymentForSmbShare(instance, instance.Namespace)
-		m.logger.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
-		err = m.client.Create(ctx, dep)
-		if err != nil {
-			m.logger.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
-			return Result{err: err}
-		}
+	deployment, created, err := m.getOrCreateDeployment(
+		ctx, instance, instance.Namespace)
+	if err != nil {
+		return Result{err: err}
+	} else if created {
 		// Deployment created successfully - return and requeue
 		return Requeue
-	} else if err != nil {
-		m.logger.Error(err, "Failed to get Deployment")
-		return Result{err: err}
 	}
 
-	// Ensure the deployment size is the same as the spec
-	var size int32 = 1
-	if *found.Spec.Replicas != size {
-		found.Spec.Replicas = &size
-		err = m.client.Update(ctx, found)
-		if err != nil {
-			m.logger.Error(err, "Failed to update Deployment", "Deployment.Namespace", found.Namespace, "Deployment.Name", found.Name)
-			return Result{err: err}
-		}
-		// Spec updated - return and requeue
+	resized, err := m.updateDeploymentSize(ctx, deployment)
+	if err != nil {
+		return Result{err: err}
+	} else if resized {
 		return Requeue
 	}
 
 	return Done
+}
+
+func (m *SmbShareManager) getOrCreateDeployment(ctx context.Context,
+	smbShare *sambaoperatorv1alpha1.SmbShare, ns string) (
+	*appsv1.Deployment, bool, error) {
+	// Check if the deployment already exists, if not create a new one
+	found := &appsv1.Deployment{}
+	err := m.client.Get(
+		ctx,
+		types.NamespacedName{
+			Name:      smbShare.Name,
+			Namespace: ns,
+		},
+		found)
+	if err == nil {
+		return found, false, nil
+	}
+
+	if errors.IsNotFound(err) {
+		// not found - define a new deployment
+		dep := m.deploymentForSmbShare(smbShare, ns)
+		m.logger.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		err = m.client.Create(ctx, dep)
+		if err != nil {
+			m.logger.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			return dep, false, err
+		}
+		// Deployment created successfully
+		return dep, true, nil
+	}
+	m.logger.Error(err, "Failed to get Deployment")
+	return nil, false, err
+}
+
+func (m *SmbShareManager) updateDeploymentSize(ctx context.Context,
+	deployment *appsv1.Deployment) (bool, error) {
+	// Ensure the deployment size is the same as the spec
+	var size int32 = 1
+	if *deployment.Spec.Replicas != size {
+		deployment.Spec.Replicas = &size
+		err := m.client.Update(ctx, deployment)
+		if err != nil {
+			m.logger.Error(err, "Failed to update Deployment", "Deployment.Namespace", deployment.Namespace, "Deployment.Name", deployment.Name)
+			return false, err
+		}
+		// Spec updated
+		return true, nil
+	}
+
+	return false, nil
 }
 
 // deploymentForSmbShare returns a smbshare deployment object
