2c1d2fcb6600229668526081d26bcb67eb193b32
John Mulligan <jmulligan@redhat.com>
2021-02-19 09:27:38 -0500

    api: add new SmbSecurityConfig type
    This adds the initial Go files that are generated by the sdk as well
    as the scaffolding the operator sdk created.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- main.go
+++ main.go
@@ -83,6 +83,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "SmbShare")
 		os.Exit(1)
 	}
+	if err = (&controllers.SmbSecurityConfigReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("SmbSecurityConfig"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "SmbSecurityConfig")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
