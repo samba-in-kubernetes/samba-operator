6311fae097d30edba5cbd4dc9395863dff1d0955
John Mulligan <jmulligan@redhat.com>
2021-03-09 18:59:24 -0500

    tests: add a share access test suite
    This is based on my previous hacky shell script approach. Currently,
    does nothing as it needs to be driven by other test code.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ tests/integration/share_access_test.go
@@ -0,0 +1,93 @@
+// +build integration
+
+package integration
+
+import (
+	"context"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/samba-in-kubernetes/samba-operator/tests/utils/kube"
+	"github.com/samba-in-kubernetes/samba-operator/tests/utils/smbclient"
+)
+
+type ShareAccessSuite struct {
+	suite.Suite
+
+	share     smbclient.Share
+	auths     []smbclient.Auth
+	clientPod string
+}
+
+func (s *ShareAccessSuite) SetupSuite() {
+	s.clientPod = "smbclient"
+
+	// ensure the smbclient test pod is configured
+	tc := kube.NewTestClient("")
+	_, err := tc.CreateFromFileIfMissing(
+		context.TODO(),
+		kube.FileSource{
+			Path:      "../files/data1.yaml",
+			Namespace: testNamespace,
+		})
+	s.Require().NoError(err)
+	_, err = tc.CreateFromFileIfMissing(
+		context.TODO(),
+		kube.FileSource{
+			Path:      "../files/client-test-pod.yaml",
+			Namespace: testNamespace,
+		})
+	s.Require().NoError(err)
+
+	// ensure the smbclient test pod exists and is ready
+	ctx, cancel := context.WithDeadline(
+		context.TODO(),
+		time.Now().Add(120*time.Second))
+	defer cancel()
+	s.Require().NoError(kube.WaitForPodExistsByLabel(
+		ctx,
+		kube.NewTestClient(""),
+		"app=samba-operator-test-smbclient",
+		testNamespace),
+		"smbclient pod does not exist",
+	)
+	s.Require().NoError(kube.WaitForPodReadyByLabel(
+		ctx,
+		kube.NewTestClient(""),
+		"app=samba-operator-test-smbclient",
+		testNamespace),
+		"smbclient pod not ready",
+	)
+}
+
+// TestLogin verifies that users can log into the share.
+func (s *ShareAccessSuite) TestLogin() {
+	smbclient := smbclient.MustPodClient(testNamespace, s.clientPod)
+	for _, auth := range s.auths {
+		err := smbclient.Command(
+			context.TODO(),
+			s.share,
+			auth,
+			[]string{"ls"})
+		s.Require().NoError(err)
+	}
+}
+
+func (s *ShareAccessSuite) TestPutFile() {
+	smbclient := smbclient.MustPodClient(testNamespace, s.clientPod)
+	auth := s.auths[0]
+	err := smbclient.Command(
+		context.TODO(),
+		s.share,
+		auth,
+		[]string{"put profile.jpeg"})
+	s.Require().NoError(err)
+	out, err := smbclient.CommandOutput(
+		context.TODO(),
+		s.share,
+		auth,
+		[]string{"ls"})
+	s.Require().NoError(err)
+	s.Require().Contains(string(out), "profile.jpeg")
+}
