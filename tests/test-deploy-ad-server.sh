31804d57b6d72abce4a2edc5cf92d80b33b65c55
Michael Adam <obnox@samba.org>
2021-03-22 00:15:43 +0100

    Add helper scripts to deploy a samba ad server
    Include patching of coredns
    
    Signed-off-by: Michael Adam <obnox@samba.org>

---
+++ tests/test-deploy-ad-server.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+
+SCRIPT_DIR="$(cd "$(dirname "${0}")" && pwd)"
+BASE_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
+DEPLOYMENT_YAML="${BASE_DIR}/tests/files/samba-ad-server-deployment.yml"
+DEPLOYMENT_NAME="samba-ad-server"
+
+_error() {
+	echo "$@"
+	exit 1
+}
+
+kubectl create -f "${DEPLOYMENT_YAML}"
+#[ $? -eq 0 ] || _error "Error creating ad server deployment"
+
+kubectl get deployment
+
+replicaset="$(kubectl describe deployment ${DEPLOYMENT_NAME} | grep -s "NewReplicaSet:" | awk '{ print $2 }')"
+[ $? -eq 0 ] || _error "Error getting replicaset"
+
+podname="$(kubectl get pod | grep $replicaset | awk '{ print $1 }')"
+[ $? -eq 0 ] || _error "Error getting podname"
+
+echo "Samba ad pod is $podname"
+
+echo "waiting for pod to be in Running state"
+tries=0
+podstatus="none"
+until [ $tries -ge 120 ] || echo $podstatus | grep -q 'Running'; do
+	sleep 1
+	echo -n "."
+	tries=$(( tries + 1 ))
+	podstatus="$(kubectl get pod $podname -o go-template='{{.status.phase}}')"
+done
+echo
+kubectl get pod
+echo
+echo $podstatus | grep -q 'Running' || _error "Pod did not reach Running state"
+
+echo "waiting for samba to become reachable"
+tries=0
+rc=1
+while [ $tries -lt 120 ] && [ $rc -ne 0 ]; do
+	sleep 1
+	tries=$(( tries + 1 ))
+	kubectl exec "${podname}" -- smbclient -N -L 127.0.0.1 2>/dev/null 1>/dev/null
+	rc=$?
+	echo -n "."
+done
+echo
+[ $rc -eq 0 ] || _error "Error: samba ad did not become reachable"
+
+
+
+AD_POD_IP=$(kubectl get pod -o jsonpath='{ .items[*].status.podIP }')
+[ $? -eq 0 ] || _error "Error getting ad server pod IP"
+
+echo "AD pod IP: ${AD_POD_IP}"
+
+TMPFILE=$(mktemp)
+
+cat > "${TMPFILE}" <<EOF
+data:
+  Corefile: |
+EOF
+
+kubectl get cm -n kube-system coredns -o jsonpath='{ .data.Corefile }' \
+	| sed -e 's/^/    /g' \
+	>> "${TMPFILE}"
+
+echo >> "${TMPFILE}"
+
+cat tests/files/coredns-snippet.template \
+	| sed -e "s/AD_SERVER_IP/${AD_POD_IP}/" \
+	>> "${TMPFILE}"
+
+echo >> "${TMPFILE}"
+
+kubectl patch cm -n kube-system coredns -p "$(cat ${TMPFILE})"
+[ $? -eq 0 ] || _error "Error patching coredns config map"
