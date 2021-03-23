54a40853e16864bcff5a6d7a2497156e43664761
John Mulligan <jmulligan@redhat.com>
2021-03-23 15:09:35 -0400

    integration tests: add a new "front end" for integration test phases
    Unlike unit tests, parts of the integration test need to be done
    in a certain order. For example, deploying the operator must be
    done before testing it can respond to CRs. Start prepping for
    that by adding a "central" test function and calling the smb share
    suites from there.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ tests/integration/integration_test.go
@@ -0,0 +1,23 @@
+// +build integration
+
+package integration
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+func runSuiteTests(sm map[string]suite.TestingSuite) func(t *testing.T) {
+	return func(t *testing.T) {
+		for name, ts := range sm {
+			t.Run(name, func(t *testing.T) {
+				suite.Run(t, ts)
+			})
+		}
+	}
+}
+
+func TestIntegration(t *testing.T) {
+	t.Run("smbShares", runSuiteTests(allSmbShareSuites()))
+}
