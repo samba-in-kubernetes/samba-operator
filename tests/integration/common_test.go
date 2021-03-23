2e4abe70b73bfe44bffb7c5fd8fef414ce00d436
John Mulligan <jmulligan@redhat.com>
2021-03-23 15:40:57 -0400

    integration tests: support specifying test files path via env var
    Previously, the path to various files used by the tests was relative
    but hard-coded. This change supports specifying this path through
    an environment variable, SMBOP_TEST_FILES_DIR. The default value
    is the same relative path as before.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/integration/common_test.go
+++ tests/integration/common_test.go
@@ -6,11 +6,20 @@ import (
 	"os"
 )
 
-var testNamespace = "samba-operator-system"
+var (
+	testNamespace = "samba-operator-system"
+
+	testFilesDir = "../files"
+)
 
 func init() {
 	ns := os.Getenv("SMBOP_TEST_NAMESPACE")
 	if ns != "" {
 		testNamespace = ns
+	}
+
+	fdir := os.Getenv("SMBOP_TEST_FILES_DIR")
+	if ns != "" {
+		testFilesDir = fdir
 	}
 }
