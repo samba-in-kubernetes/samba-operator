c0e3e5cbe16e5f5e074eae89c299b05cd6be64a5
John Mulligan <jmulligan@redhat.com>
2021-04-21 10:18:15 -0400

    main: add version and commit id vars to binary build
    These can be used to help identify the provenance of the binary.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- main.go
+++ main.go
@@ -36,6 +36,11 @@ import (
 var (
 	scheme   = runtime.NewScheme()
 	setupLog = ctrl.Log.WithName("setup")
+
+	// Version of the software at compile time.
+	Version = "(unset)"
+	// CommitID of the revision used to compile the software.
+	CommitID = "(unset)"
 )
 
 func init() {
@@ -93,7 +98,9 @@ func main() {
 	}
 	// +kubebuilder:scaffold:builder
 
-	setupLog.Info("starting manager")
+	setupLog.Info("starting manager",
+		"Version", Version,
+		"CommitID", CommitID)
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
