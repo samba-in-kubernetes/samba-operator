cf1f51a05ce0be8810f824f31fd7f68bc0858323
John Mulligan <jmulligan@redhat.com>
2021-10-13 16:02:46 -0400

    test utils: add funcs for waiting on pods using fetch opts
    These new functions pair with the new FetchPods and PodFetchOptions
    and will eventually replace the old Wait*ByLabel versions.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/utils/kube/client.go
+++ tests/utils/kube/client.go
@@ -161,3 +161,63 @@ func WaitForPodExistsByLabel(
 	}
 	return nil
 }
+
+func waitFor(ctx context.Context, cond func() (bool, error)) error {
+	for {
+		c, err := cond()
+		if err != nil {
+			return err
+		}
+		if c {
+			break
+		}
+		if err := ctx.Err(); err != nil {
+			return err
+		}
+		time.Sleep(200 * time.Millisecond)
+	}
+	return nil
+}
+
+// WaitForAnyPodReady will wait for a pod to be ready, up to the deadline
+// specified by the context, if the context lacks a deadline the call will
+// block indefinitely. Pods are selected using the PodFetchOptions.
+func WaitForAnyPodReady(
+	ctx context.Context, tc *TestClient, fo PodFetchOptions) error {
+	// ---
+	return waitFor(
+		ctx,
+		func() (bool, error) {
+			pods, err := tc.FetchPods(ctx, fo)
+			if err != nil {
+				return false, err
+			}
+			for _, pod := range pods {
+				if PodIsReady(&pod) {
+					return true, nil
+				}
+			}
+			return false, nil
+		},
+	)
+}
+
+// WaitForAnyPodExists will wait for a pod to exist, up to the deadline
+// specified by the context, if the context lacks a deadline the call will
+// block indefinitely. Pods are selected using the PodFetchOptions.
+func WaitForAnyPodExists(
+	ctx context.Context, tc *TestClient, fo PodFetchOptions) error {
+	// ---
+	return waitFor(
+		ctx,
+		func() (bool, error) {
+			_, err := tc.FetchPods(ctx, fo)
+			if err == nil {
+				return true, nil
+			} else if errors.Is(err, ErrNoMatchingPods) {
+				return false, nil
+			}
+			return false, err
+		},
+	)
+}
