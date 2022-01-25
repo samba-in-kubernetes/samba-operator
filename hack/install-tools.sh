3cb40b8526fd592a23782de890cd2b9c918aeb23
Shachar Sharon <ssharon@redhat.com>
2022-01-25 11:38:39 +0200

    hack: helper script to install auxiliary build-tools
    Wrap 'go install' with helper script, which facilitate the installation
    of helper build tools, with (optional) alternative GOBIN to user's defaults.
    
    For the special case of 'kustomize', build the tool within a temporary
    directory and install using 'go get'.
    
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

---
+++ hack/install-tools.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+# helper script to install build auxiliary tools in local directory
+#
+# usage:
+#   GOBIN=<dir> install-tools.sh --<tool-name>
+#
+set -e
+GO_CMD=${GO_CMD:-$(command -v go)}
+GOBIN=${GOBIN:-$(pwd)/bin}
+
+_require_gobin() {
+	mkdir -p "${GOBIN}"
+}
+
+_install_kustomize() {
+	curdir="$(pwd)"
+	tmpdir="$(mktemp -d)"
+
+	mkdir -p "${tmpdir}"
+	cd "${tmpdir}"
+	${GO_CMD} get sigs.k8s.io/kustomize/kustomize/v4@v4.3.0
+	cd "${curdir}"
+	find "${tmpdir}" -type d -exec chmod 700 {} \;
+	rm -rf "${tmpdir}"
+}
+
+_install_tool() {
+	GOBIN="${GOBIN}" ${GO_CMD} install "$1"
+}
+
+_install_controller_gen() {
+	_install_tool sigs.k8s.io/controller-tools/cmd/controller-gen@v0.6.2
+}
+
+_install_revive() {
+	_install_tool github.com/mgechev/revive@latest
+}
+
+_install_golangci_lint() {
+	_install_tool github.com/golangci/golangci-lint/cmd/golangci-lint@v1.43.0
+}
+
+_install_yq() {
+	_install_tool github.com/mikefarah/yq/v4@latest
+}
+
+case "$1" in
+	--kustomize)
+		_require_gobin
+		_install_kustomize
+		;;
+	--controller-gen)
+		_require_gobin
+		_install_controller_gen
+		;;
+	--revive)
+		_require_gobin
+		_install_revive
+		;;
+	--golangci-lint)
+		_require_gobin
+		_install_golangci_lint
+		;;
+	--yq)
+		_require_gobin
+		_install_yq
+		;;
+	*)
+		echo "usage: GOBIN=<dir> $0 --<tool-name>"
+		echo ""
+		echo "availabel tools:"
+		echo "  --kustomize"
+		echo "  --controller-gen"
+		echo "  --revive"
+		echo "  --golangci-lint"
+		echo "  --yq"
+		;;
+esac
