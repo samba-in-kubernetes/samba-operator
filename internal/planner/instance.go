a7b0cf82142a871930336e2d9528e353289acc66
John Mulligan <jmulligan@redhat.com>
2022-03-09 15:36:17 -0500

    planner: move miscellaneous functions to misc.go
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- internal/planner/instance.go
+++ internal/planner/instance.go
@@ -21,27 +21,6 @@ import (
 	"github.com/samba-in-kubernetes/samba-operator/internal/smbcc"
 )
 
-type dnsRegister string
-
-const (
-	dnsRegisterNever      = dnsRegister("never")
-	dnsRegisterExternalIP = dnsRegister("external-ip")
-	dnsRegisterClusterIP  = dnsRegister("cluster-ip")
-)
-
-// "cheat codes"
-const (
-	nodeSpreadKey     = "samba-operator.samba.org/node-spread"
-	nodeSpreadDisable = "false"
-)
-
-type userSecuritySource struct {
-	Configured bool
-	Namespace  string
-	Secret     string
-	Key        string
-}
-
 // InstanceConfiguration bundles together the various inputs that define
 // the configuration of a server group instance.
 type InstanceConfiguration struct {
@@ -74,57 +53,4 @@ func New(
 		InstanceConfiguration: ic,
 		ConfigState:           state,
 	}
-}
-
-func (sp *Planner) userSecuritySource() userSecuritySource {
-	s := userSecuritySource{}
-	if sp.securityMode() != userMode {
-		return s
-	}
-	if sp.SecurityConfig == nil || sp.SecurityConfig.Spec.Users == nil {
-		return s
-	}
-	s.Configured = true
-	s.Namespace = sp.SecurityConfig.Namespace
-	s.Secret = sp.SecurityConfig.Spec.Users.Secret
-	s.Key = sp.SecurityConfig.Spec.Users.Key
-	return s
-}
-
-func (sp *Planner) dnsRegister() dnsRegister {
-	reg := dnsRegisterNever
-	if sp.securityMode() == adMode && sp.SecurityConfig.Spec.DNS != nil {
-		reg = dnsRegister(sp.SecurityConfig.Spec.DNS.Register)
-	}
-	switch reg {
-	// allowed values
-	case dnsRegisterExternalIP, dnsRegisterClusterIP:
-	// anything else is reverted to "never"
-	case dnsRegisterNever:
-		fallthrough
-	default:
-		reg = dnsRegisterNever
-	}
-	return reg
-}
-
-func (sp *Planner) serviceType() string {
-	if sp.CommonConfig != nil && sp.CommonConfig.Spec.Network.Publish == "external" {
-		return "LoadBalancer"
-	}
-	return "ClusterIP"
-}
-
-func (sp *Planner) sambaContainerDebugLevel() string {
-	return sp.GlobalConfig.SambaDebugLevel
-}
-
-func (sp *Planner) mayCluster() bool {
-	return sp.GlobalConfig.ClusterSupport == "ctdb-is-experimental"
-}
-
-// nodeSpread returns true if pods are required to be spread over multiple
-// nodes.
-func (sp *Planner) nodeSpread() bool {
-	return sp.SmbShare.Annotations[nodeSpreadKey] != nodeSpreadDisable
 }
