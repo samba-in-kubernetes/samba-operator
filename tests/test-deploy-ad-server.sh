f58f13c2d733aa9878ffaec05be4a6053557f64a
Shachar Sharon <ssharon@redhat.com>
2022-04-12 18:31:27 +0300

    tests: allow passing KUBECTL_CMD to deploy AD script
    On OpenShift deployments users may want to use 'oc' as their kubectl
    command. Allow passing alternative command via 'KUBECTL_CMD' variable
    (same as in top-level Makefile).
    
    Made also few cosmetics fixes to cleanup 'shellcheck' warns and align to
    shell-scripts coding conventions.
    
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

--- tests/test-deploy-ad-server.sh
+++ tests/test-deploy-ad-server.sh
@@ -6,6 +6,7 @@ BASE_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
 DEPLOYMENT_YAML="${BASE_DIR}/tests/files/samba-ad-server-deployment.yml"
 DEPLOYMENT_NAME="samba-ad-server"
 COREDNS_SNIPPET="${BASE_DIR}/tests/files/coredns-snippet.template"
+KUBECTL_CMD=${KUBECTL_CMD:-kubectl}
 
 _error() {
 	echo "$@"
@@ -13,7 +14,7 @@ _error() {
 }
 
 echo "Creating ad server deployment..."
-ERROR_MSG=$(kubectl create -f "${DEPLOYMENT_YAML}" 2>&1 1>/dev/null)
+ERROR_MSG=$(${KUBECTL_CMD} create -f "${DEPLOYMENT_YAML}" 2>&1 1>/dev/null)
 if [ $? -ne 0 ] ; then
 	if [[ "${ERROR_MSG}" =~ "AlreadyExists" ]] ; then
 		echo "Deployment exists already. Continuing."
@@ -22,12 +23,13 @@ if [ $? -ne 0 ] ; then
 	fi
 fi
 
-kubectl get deployment
+${KUBECTL_CMD} get deployment
 
-replicaset="$(kubectl describe deployment ${DEPLOYMENT_NAME} | grep -s "NewReplicaSet:" | awk '{ print $2 }')"
+replicaset="$(${KUBECTL_CMD} describe deployment ${DEPLOYMENT_NAME} | \
+	grep -s "NewReplicaSet:" | awk '{ print $2 }')"
 [ $? -eq 0 ] || _error "Error getting replicaset"
 
-podname="$(kubectl get pod | grep $replicaset | awk '{ print $1 }')"
+podname="$(${KUBECTL_CMD} get pod | grep "${replicaset}" | awk '{ print $1 }')"
 [ $? -eq 0 ] || _error "Error getting podname"
 
 echo "Samba ad pod is $podname"
@@ -39,12 +41,14 @@ until [ $tries -ge 120 ] || echo $podstatus | grep -q 'Running'; do
 	sleep 1
 	echo -n "."
 	tries=$(( tries + 1 ))
-	podstatus="$(kubectl get pod $podname -o go-template='{{.status.phase}}')"
+	podstatus="$(${KUBECTL_CMD} get pod "${podname}" \
+		-o go-template='{{.status.phase}}')"
 done
 echo
-kubectl get pod
+${KUBECTL_CMD} get pod
 echo
-echo $podstatus | grep -q 'Running' || _error "Pod did not reach Running state"
+echo "${podstatus}" | grep -q 'Running' || \
+	_error "Pod did not reach Running state"
 
 echo "waiting for samba to become reachable"
 tries=0
@@ -52,7 +56,8 @@ rc=1
 while [ $tries -lt 120 ] && [ $rc -ne 0 ]; do
 	sleep 1
 	tries=$(( tries + 1 ))
-	kubectl exec "${podname}" -- smbclient -N -L 127.0.0.1 2>/dev/null 1>/dev/null
+	${KUBECTL_CMD} exec "${podname}" -- \
+		smbclient -N -L 127.0.0.1 2>/dev/null 1>/dev/null
 	rc=$?
 	echo -n "."
 done
@@ -61,7 +66,7 @@ echo
 
 
 
-AD_POD_IP=$(kubectl get pod -o jsonpath='{ .items[*].status.podIP }')
+AD_POD_IP=$(${KUBECTL_CMD} get pod -o jsonpath='{ .items[*].status.podIP }')
 [ $? -eq 0 ] || _error "Error getting ad server pod IP"
 
 echo "AD pod IP: ${AD_POD_IP}"
@@ -73,7 +78,7 @@ data:
   Corefile: |
 EOF
 
-kubectl get cm -n kube-system coredns -o jsonpath='{ .data.Corefile }' \
+${KUBECTL_CMD} get cm -n kube-system coredns -o jsonpath='{ .data.Corefile }' \
 	| sed -e 's/^/    /g' \
 	>> "${TMPFILE}"
 
@@ -83,11 +88,11 @@ echo >> "${TMPFILE}"
 FIRSTLINE="$(head -1 "${COREDNS_SNIPPET}")"
 LASTLINE="    }"
 
-sed -i.backup -e "/$FIRSTLINE/,/$LASTLINE/d" ${TMPFILE}
+sed -i.backup -e "/$FIRSTLINE/,/$LASTLINE/d" "${TMPFILE}"
 
 sed -e "s/AD_SERVER_IP/${AD_POD_IP}/" "${COREDNS_SNIPPET}" >> "${TMPFILE}"
 
 echo >> "${TMPFILE}"
 
-kubectl patch cm -n kube-system coredns -p "$(cat ${TMPFILE})"
+${KUBECTL_CMD} patch cm -n kube-system coredns -p "$(cat "${TMPFILE}")"
 [ $? -eq 0 ] || _error "Error patching coredns config map"
