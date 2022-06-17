79cf44c6f272818b46026ea2332cd1b538741761
John Mulligan <jmulligan@redhat.com>
2022-06-17 17:52:54 -0400

    integration tests: add new TestGroup type
    The TestGroup will serve as a future replacement for our current
    map-based approach to running multiple test suites. The plan is
    to have something that is deterministic by default and somewhat
    simple to use. It's clearly more code than the old function but
    the idea is that the various files defining tests will only
    need to interact with the obvious parts of this type.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -19,6 +19,49 @@ func runSuiteTests(sm map[string]suite.TestingSuite) func(t *testing.T) {
 	}
 }
 
+type namedSuite struct {
+	name      string
+	testSuite suite.TestingSuite
+}
+
+type TestingGroup interface {
+	Name() string
+	Run(*testing.T)
+}
+
+type TestGroup struct {
+	name     string
+	children []TestingGroup
+	suites   []namedSuite
+}
+
+func (tg *TestGroup) Run(t *testing.T) {
+	for _, s := range tg.suites {
+		t.Run(s.name, func(t *testing.T) {
+			suite.Run(t, s.testSuite)
+		})
+	}
+	for _, child := range tg.children {
+		t.Run(child.Name(), child.Run)
+	}
+}
+
+func (tg *TestGroup) Name() string {
+	return tg.name
+}
+
+func (tg *TestGroup) Child(name string) *TestGroup {
+	child := &TestGroup{name: name}
+	tg.children = append(tg.children, child)
+	return child
+}
+
+func (tg *TestGroup) AddSuite(n string, s suite.TestingSuite) {
+	tg.suites = append(tg.suites, namedSuite{name: n, testSuite: s})
+}
+
+var testRoot *TestGroup = &TestGroup{}
+
 func TestIntegration(t *testing.T) {
 	t.Run("deploy", runSuiteTests(allDeploySuites()))
 	t.Run("smbShares", runSuiteTests(allSmbShareSuites()))
