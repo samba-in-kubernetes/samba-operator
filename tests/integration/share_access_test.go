88cfd6df8c680c5e33aeab42bbf4bd7279850be3
John Mulligan <jmulligan@redhat.com>
2022-06-18 10:28:27 -0400

    integration tests: context passing from smb shares to share access suites
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/integration/share_access_test.go
+++ tests/integration/share_access_test.go
@@ -18,24 +18,39 @@ type ShareAccessSuite struct {
 	share     smbclient.Share
 	auths     []smbclient.Auth
 	clientPod string
+
+	// parentContext is a context provided by this test's parent.
+	// some would consider this not a best practice, but it is
+	// a simple way to know what smbshare/etc is under test.
+	// We always assume this suite is executed by a higher level test
+	parentContext context.Context
+}
+
+func (s *ShareAccessSuite) defaultContext() context.Context {
+	if s.parentContext == nil {
+		// fallback in case parentContext is unset
+		return testContext()
+	}
+	return s.parentContext
 }
 
 func (s *ShareAccessSuite) SetupSuite() {
 	s.clientPod = "smbclient"
 
 	// ensure the smbclient test pod is configured
-	createSMBClientIfMissing(context.TODO(), s.Require(), kube.NewTestClient(""))
+	createSMBClientIfMissing(s.defaultContext(), s.Require(), kube.NewTestClient(""))
 }
 
 // TestLogin verifies that users can log into the share.
 func (s *ShareAccessSuite) TestLogin() {
+	ctx := s.defaultContext()
 	tc := kube.NewTestClient("")
 	smbclient := smbclient.MustPodExec(tc, testNamespace, s.clientPod, "")
-	err := smbclient.CacheFlush(context.TODO())
+	err := smbclient.CacheFlush(ctx)
 	s.Require().NoError(err)
 	for _, auth := range s.auths {
 		err := smbclient.Command(
-			context.TODO(),
+			ctx,
 			s.share,
 			auth,
 			[]string{"ls"})
@@ -44,19 +59,20 @@ func (s *ShareAccessSuite) TestLogin() {
 }
 
 func (s *ShareAccessSuite) TestPutFile() {
+	ctx := s.defaultContext()
 	tc := kube.NewTestClient("")
 	smbclient := smbclient.MustPodExec(tc, testNamespace, s.clientPod, "")
-	err := smbclient.CacheFlush(context.TODO())
+	err := smbclient.CacheFlush(ctx)
 	s.Require().NoError(err)
 	auth := s.auths[0]
 	err = smbclient.Command(
-		context.TODO(),
+		ctx,
 		s.share,
 		auth,
 		[]string{"put profile.jpeg"})
 	s.Require().NoError(err)
 	out, err := smbclient.CommandOutput(
-		context.TODO(),
+		ctx,
 		s.share,
 		auth,
 		[]string{"ls"})
