3dc4610f8a24b3b5e3aadd79d7330bc4a0b8ecff
John Mulligan <jmulligan@redhat.com>
2023-03-02 16:20:38 -0500

    integration tests: add env var SMBOP_TEST_MIN_NODE_COUNT
    Add yet another environment variable to inform the test suite about the
    outer world. The variable SMBOP_TEST_MIN_NODE_COUNT should be set to the
    number of nodes that typical pods may be scheduled on, the tests may
    then use this value to determine if the cluster is in-spec or
    out-of-spec for tests that require a certain number of nodes.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/integration/common_test.go
+++ tests/integration/common_test.go
@@ -4,6 +4,7 @@
 package integration
 
 import (
+	"fmt"
 	"math/rand"
 	"os"
 	"strconv"
@@ -23,6 +24,8 @@ var (
 	testClusteredShares = false
 
 	testShuffleOrder = false
+
+	testMinNodeCount = 0
 )
 
 func init() {
@@ -63,6 +66,20 @@ func init() {
 	shuffleEnv := os.Getenv("SMBOP_TEST_SHUFFLE")
 	if b, err := strconv.ParseBool(shuffleEnv); b && err == nil {
 		testShuffleOrder = true
+	}
+
+	// specify the number of nodes available for the test run. Some tests
+	// require >1 node or >N nodes to function, so this can be used to decide
+	// what to run in those cases. This should only specify the number of
+	// "worker" nodes that can run samba pods - it should not include control
+	// plane nodes that are not schedulable by default.
+	nc := os.Getenv("SMBOP_TEST_MIN_NODE_COUNT")
+	if nc != "" {
+		if v, err := strconv.ParseInt(nc, 10, 32); err == nil {
+			testMinNodeCount = int(v)
+		} else {
+			fmt.Printf("Failed to convert %s to an int\n", nc)
+		}
 	}
 
 	// ensure that tests run with a random seed. This can be removed once
